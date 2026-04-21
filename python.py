5ba5de73ef4b105d1c37a4d5fa0105f51cfe6042
Avishkar Chavhan <avishkarchavhan45@gmail.com>
2026-04-21 11:44:34 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,10 @@
-print("my name is Avishkar")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "My name is Avishkar"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
